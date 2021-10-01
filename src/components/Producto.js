008fbee4c8739397b89bfe674102b74dd715eea3
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-10-01 10:43:46 -0500

    Se dieron estilos a los componentes de productos y producto

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,12 +1,25 @@
 import { Component } from 'react';
 
+const styles = {
+   producto: {
+      border: '1px solid #eee',
+      boxShadow: '0 5px 5px rgba(0, 0, 0, 0.1)',
+      width: '30%',
+      padding: '10px, 15px',
+      borderRadius: '5px',
+   },
+   img: {
+      width: '100%',
+   },
+};
+
 class Producto extends Component {
    render() {
       const { producto } = this.props;
       console.log(this.props);
       return (
-         <div>
-            <img alt={producto.name} src={producto.img} />
+         <div style={styles.producto}>
+            <img style={styles.img} alt={producto.name} src={producto.img} />
             <h3>{producto.name}</h3>
             <p>${producto.price}</p>
          </div>
