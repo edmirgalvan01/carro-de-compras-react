d5d6602661dcb4b1e38d4024f8dff8f99ee1cabe
edmirgalvan01 <edmirgalvaz@gmail.com>
2021-10-04 12:42:02 -0500

    Se creo la logica de la funcion agreagaAlCarro

--- src/App.js
+++ src/App.js
@@ -11,15 +11,41 @@ class App extends Component {
          { name: 'Arvejas', price: 15, img: '/productos/arbejas.jpg' },
          { name: 'Lechuga', price: 30, img: '/productos/lechuga.jpg' },
       ],
+      carro: [],
    };
+
+   agregarAlCarro = (producto) => {
+      const { carro } = this.state;
+
+      if (carro.find((x) => x.name === producto.name)) {
+         const newCarro = carro.map((x) =>
+            x.name === producto.name
+               ? {
+                    ...producto,
+                    cantidad: x.cantidad + 1,
+                 }
+               : x
+         );
+         return this.setState({ carro: newCarro });
+      }
+
+      return this.setState({
+         carro: this.state.carro.concat({
+            ...producto,
+            cantidad: 1,
+         }),
+      });
+   };
+
    render() {
+      console.log(this.state.carro);
       return (
          <div>
             <Navbar />
             <Layout>
                <Title />
                <Productos
-                  agregarAlCarro={() => console.log('No hace nada')}
+                  agregarAlCarro={this.agregarAlCarro}
                   productos={this.state.productos}
                />
             </Layout>
